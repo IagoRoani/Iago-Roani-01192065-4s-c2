cbb2f16f96a736d246da55cfda2db3c77f3a2495
IagoRoani <Iago.lima@bandtec.com.br>
2021-04-14 20:18:04 -0300

    Cria data class e de conexão com a api

--- AvaliaoContinuada2/app/src/main/java/com/example/avaliaocontinuada2/ApiCachorros.kt
+++ AvaliaoContinuada2/app/src/main/java/com/example/avaliaocontinuada2/ApiCachorros.kt
@@ -1,4 +1,12 @@
 package com.example.avaliaocontinuada2
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface ApiCachorros {
+
+    @GET("filmes/{id}")
+    fun get(@Path("id") id:Int): Call<Cachorro>
+
 }
