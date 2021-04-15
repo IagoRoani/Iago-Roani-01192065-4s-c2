161977d71720c62e38c7a1c320c06701f6106cb3
IagoRoani <Iago.lima@bandtec.com.br>
2021-04-14 21:10:32 -0300

    Cria classes restantes e começo da lógica para funcionar conexão e tranferencias de dados

--- AvaliaoContinuada2/app/src/main/java/com/example/avaliaocontinuada2/MainActivity.kt
+++ AvaliaoContinuada2/app/src/main/java/com/example/avaliaocontinuada2/MainActivity.kt
@@ -1,16 +1,60 @@
 package com.example.avaliaocontinuada2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.Switch
+import android.widget.TextView
+import android.widget.Toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnComprar : Button
+    lateinit var swSeguranca : Switch
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnComprar = findViewById(R.id.btn_comprar)
+        swSeguranca = findViewById(R.id.sw_seguranca)
+
     }
 
     fun resultado(view: View) {
+
+        if (swSeguranca.isChecked){
+            val telaSucesso = Intent(this, SucessoEncontrar::class.java)
+            startActivity(telaSucesso)
+
+            val apiCachorros = ConexaoApiCachorros.criar()
+
+            apiCachorros.get().enqueue(object : Callback<List<Cachorro>> {
+
+                // onResponse é executando se chamada for feita com sucesso
+                override fun onResponse(call: Call<List<Cachorro>>, response: Response<List<Cachorro>>) {
+
+                }
+
+                // onFailure é executado se houver erro na chamada
+                override fun onFailure(call: Call<List<Filme>>, t: Throwable) {
+                    Log.e("api", t.message!!)
+                    // t.printStackTrace()
+                    Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
+                }
+            })
+
+        }
+
+        if (!swSeguranca.isChecked){
+
+        }
 
     }
 }
---
+++ AvaliaoContinuada2/app/src/main/java/com/example/avaliaocontinuada2/SucessoEncontrar.kt
@@ -0,0 +1,11 @@
+package com.example.avaliaocontinuada2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SucessoEncontrar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sucesso_encontrar)
+    }
+}
